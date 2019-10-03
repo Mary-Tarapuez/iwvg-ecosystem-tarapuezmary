83c21eba52b841502074c79134d3e22dea38f8db
Mary-Tarapuez <55232443+Mary-Tarapuez@users.noreply.github.com>
2019-10-03 12:55:06 +0200

    Add UserTest

---
+++ src/test/java/practica/UserTest.java
@@ -0,0 +1,39 @@
+package practica;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    private User user1;
+
+    @BeforeEach
+    void before() {
+        user1 = new User(3, "verdana", "Font family");
+    }
+
+    @Test
+    public void testFullName() {
+        String valor = user1.fullName();
+        Assert.assertEquals("Verdana Font family", valor);
+    }
+
+    @Test
+    public void testGetNumber() {
+        int number = user1.getNumber();
+        Assert.assertEquals(3, number);
+    }
+
+    @Test
+    public void testGetName() {
+        String name = user1.getName();
+        Assert.assertEquals("Verdana", name);
+    }
+
+    @Test
+    public void testGetFamilyName() {
+        String familyName = user1.getFamilyName();
+        Assert.assertEquals("Font family", familyName);
+    }
+
+}
