a147be96dcd157ca167726f03e81100aabeae5ac
Mary-Tarapuez <55232443+Mary-Tarapuez@users.noreply.github.com>
2019-10-03 17:34:12 +0200

    Point Mejora1 issue#11

--- src/test/java/practica/PointTest.java
+++ src/test/java/practica/PointTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PointTest {
     private Point point;
 
+    private Point pointz;
+
     @BeforeEach
     void before() {
         point = new Point(2, 3);
@@ -41,6 +43,20 @@ class PointTest {
     @Test
     void testPhase() {
         assertEquals(0.9828, point.phase(), 10e-5);
+    }
+
+    @Test
+    void testModulez() {
+        pointz = new Point(2, 3, 4);
+        double resultado = pointz.modulez();
+        assertEquals(5.3851, resultado, 10e-5);
+    }
+
+    @Test
+    void testPhasez() {
+        pointz = new Point(2, 20, 1);
+        double resultado = pointz.phasez();
+        assertEquals(1.4711276743037347, resultado, 10e-5);
     }
 
     @Test
